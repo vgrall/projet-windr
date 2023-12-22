bec00842443b61ef3870c7eecffccd5639937379
virginie <virginie.grall@gmail.com>
2023-12-22 15:00:57 +0100

    banniereV2

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="banner-container">
+      {/* <img
+        id="backgroundBanner"
+        src="images/backgroundBanner.jpeg"
+        alt="backgroundBanner"
+      />
+      <img id="windr" src="images/logowindr.png" alt="banner" /> */}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  return (
+    <div>
+      <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+        <div className={`navbar_logo ${showLinks ? "hide-logo" : ""}`}>
+          <img id="windr" src="images/logowindr.png" alt="logo" />
+        </div>
+        <ul className="navbar_links">
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Accueil
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Classements
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Riders
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Catalogue
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Documentation
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Nouveautés
+            </a>
+          </li>
+          <li className="navbar_item">
+            <a className="navbar_link" href="/">
+              Connexion
+            </a>
+          </li>
+        </ul>
+        <button className="navbar_burger" onClick={handleShowLinks}>
+          <span className="burger-bar"></span>
+        </button>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
